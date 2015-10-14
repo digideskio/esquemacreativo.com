10d6a4be9905f8f3ca90a257e3ba0edc482eaaea
CamiloRuiiz <camiloruiz@esquemacreativo.com>
2015-10-14 10:39:37 +0200

    cambiando index

--- index-old.php
+++ index-old.php
@@ -1,140 +1,51 @@
-<!doctype html>
-<html>
-  <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author" content="">
+<?php require_once('content/php/easily.php'); ?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> 
+<html class="ie ie6 no-js" lang="en">
+    <![endif]-->
+    <!--[if IE 7 ]>    
+    <html class="ie ie7 no-js" lang="en">
+        <![endif]-->
+        <!--[if IE 8 ]>    
+        <html class="ie ie8 no-js" lang="en">
+            <![endif]-->
+            <!--[if IE 9 ]>    
+            <html class="ie ie9 no-js" lang="en">
+                <![endif]-->
+                <!--[if gt IE 9]><!-->
+                <html lang="es" xml:lang="es" xmlns="http://www.w3.org/1999/xhtml">
+                    <!--<![endif]-->
+                    <?php require_once('nodes/head.php'); ?>
+                    <body>
+                        <div id="pageloader">
+                            <div class="loader-item">
+                                <img src="content/img/loading.gif" alt='loader' />
+                            </div>
+                        </div>
+                        <!--end:preloader-->
+                        <!--wrapper-->
+                        <div class="wrapper">
+                        <?php require_once('nodes/header.php'); ?>
+                        <?php
+				
+							$file = "nodes/".$_REQUEST['content'].".php";
+							
+							if(!isset($_REQUEST['content'])){
+								
+								require "nodes/home.php";
+								
+							}elseif(file_exists($file)){
+								
+								require $file;
+								
+							}else{
+								
+								require "nodes/404.php";
+							}
+							
+							?>
+                        <?php require_once('nodes/modals.php'); ?>
+                        <?php require_once('nodes/footer.php'); ?>
+                    </body>
+                </html>
 
-	<title>GPUX</title>
-
-	<!-- Bootstrap core CSS -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-    
-	<!-- Style CSS -->
-	<link href="style.css" rel="stylesheet">
-    <!-- Customize CSS -->
-	<link href="content/css/custom.css" rel="stylesheet">
-    <!-- Responsive CSS -->
-	<link href="content/css/responsive.css" rel="stylesheet">
-    
-    
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	  <script src="assets/js/html5shiv.js"></script>
-	  <script src="assets/js/respond.min.js"></script>
-	<![endif]-->
-
-	<!-- Favicons -->
-	<link rel="shortcut icon" href="placeholders/ico/favicon.ico" type="image/x-icon">
-	<link rel="apple-touch-icon" href="placeholders/ico/apple-touch-icon.png" />
-	<link rel="apple-touch-icon" sizes="57x57" href="placeholders/ico/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="placeholders/ico/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="placeholders/ico/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="placeholders/ico/apple-touch-icon-144x144.png">
-  </head>
-  <body>
-	<div class="under-wrap slide-images">
-    	<div class="pattern"></div>
-        
-    <div class="under-content middle-content">
-    	<div class="logo"><img src="content/placeholders/main-logo.png"></div>
-        <h1>.:: algo impresionante viene ::.</h1>
-        <h5>el lanzamiento de un nuevo sitio en breve!</h5>
-    </div><!--end:under-content-->
-    
-    <div class="bottom-info">
-    	<!-- Start newsletter -->
-        <h5 style="color: #FFF;">Te mantendremos informados!</h5>
-        <div class="newsletter">
-            <!--
-            <form id="news" class="news">
-            <div class="email-wrapper">
-                <input type="text" id="email" name="email" value="Escribe tu Correo" required=""/>
-            </div>
-            <div class="submit-wrapper">
-                <input type="hidden" id="consulta" name="consulta" value="news" />
-                <input type="submit" id="sub-news" name="sub-news" value="Enviar"/>
-            </div>
-            </form> -->
-        </div>
-        <!---footer--->
-        <p>Copyright 2015 <strong>GPUX</strong> - Diseñado por <strong>GrowthPoint.es</strong>. <br> Powered by <strong>Html5 & Css3</strong></p>
-    </div>
-    
-    
-        <div id="under-slides">
-        <div class="slides-container">
-          <img src="content/placeholders/backgrounds/bg-16.jpg" >
-          <img src="content/placeholders/backgrounds/bg-17.jpg" >
-          <img src="content/placeholders/backgrounds/bg-18.jpg">
-        </div>
-    
-        <nav class="slides-navigation">
-          <a href="#" class="next">Next</a>
-          <a href="#" class="prev">Previous</a>
-        </nav>
-      </div>
-  
-  
-  
-        
-    </div>
-
-<!--jQuery-->
-	<script type="text/javascript" src="content/js/jquery-1.10.2.min.js"></script>
-<!--Bootstrap-->    
-	<script type="text/javascript" src="content/js/bootstrap.min.js"></script>
-<!--Parallax-->
-	<script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.validation/1.14.0/jquery.validate.js"></script>
-<!--Fancy Select-->
-	<script type="text/javascript" src="content/js/fancySelect.js"></script>
-<!--jQuery Ui-->
-	<script type="text/javascript" src="content/js/jquery-ui.js"></script>
-<!--Pretty Photo-->
-	<script type="text/javascript" src="content/js/jquery.prettyPhoto.js"></script>
-<!--Header Sticky -->
-    <script type="text/javascript" src="content/js/waypoints.min.js" ></script>
-<!--Mobile Menu -->    
-	<script type="text/javascript" src="content/js/modernizr.custom.js"></script>
-	<script type="text/javascript" src="content/js/jquery.dlmenu.js"></script>
-<!--Owl Carousel-->
-	<script type="text/javascript" src="content/js/owl.carousel.js"></script>
-<!--iSotope-->
-	<script type="text/javascript" src="content/js/isotope.pkgd.min.js"></script>
-<!--Circle Chart-->
-	<script type="text/javascript" src="content/js/jquery.circliful.min.js"></script>         
-<!--scrollReveal-->
-	<script type="text/javascript" src="content/js/scrollReveal.js"></script>
-<!--Html5 player-->    
-    <script type="text/javascript" src="content/js/mediaelement-and-player.min.js"></script>
-<!--Countdown--> 
-    <script type="text/javascript" src="content/js/TimeCircles.js"></script>
-<!--Counter Up-->     
-    <script type="text/javascript" src="content/js/jquery-counterup.js"></script> 
-<!-- Responsive video -->
-	<script type="text/javascript" src="content/js/jquery.fitvids.js"></script>
-<!--Flat Surface Shader -->     
-    <script type="text/javascript" src="content/js/fss.js"></script>
-<!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
-    <script type="text/javascript" src="content/js/jquery.themepunch.plugins.min.js"></script>
-    <script type="text/javascript" src="content/js/jquery.themepunch.revolution.min.js"></script>    
-<!--Onepage scroll-->     
-    <script type="text/javascript" src="content/js/jQuery-scrollnav.js"></script>
-<!--Fullscreen slide-->     
-    <script type="text/javascript" src="content/js/jquery.superslides.js"></script>            
-<!--Background video-->     
-    <script type="text/javascript" src="content/js/jquery.mb.YTPlayer.js"></script>     
-<!--Retina support-->
-	<script type="text/javascript" src="content/js/retina.js"></script>
-<!--Main Script-->
-	<script type="text/javascript" src="content/js/main.js"></script> <!--custom script-->
-	<script>
-    $('#under-slides').superslides({
-      animation: 'fade',
-	  hashchange: true,
-      play: 4000
-    });
-  </script>    
-    </body>
-</html>
